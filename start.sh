f997abf2d76499847ac897664ca133ce93e00ee5
mofeng-git <mofeng654321@hotmail.com>
2025-06-06 02:38:14 +0000

    添加编译启动脚本和内核 dvd 仿真补丁

---
+++ start.sh
@@ -0,0 +1,8 @@
+ #!/bin/bash
+ ./compile.sh build BOARD=onecloud BRANCH=legacy BUILD_DESKTOP=no BUILD_MINIMAL=yes \
+    COMPRESS_OUTPUTIMAGE=img EXTRAWIFI=yes  KERNEL_CONFIGURE=no  DOWNLOAD_MIRROR=china MAINLINE_MIRROR=tuna \
+    RELEASE=jammy BOOTSIZE=64  SYNC_CLOCK=no INCLUDE_HOME_DIR=yes USE_CCACHE=yes ALLOW_ROOT=yes PESTER_TERMINAL=no \
+    KERNEL_COMPILER=/opt/gcc-arm-9.2-2019.12-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-
+
+./userpatches/burnimg-pack.sh output/images/Armbian_by-SilentWind_24.5.0-trunk_Onecloud_jammy_legacy_5.9.0-rc7_minimal.img
+
